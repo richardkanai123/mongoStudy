52450fc7c85c31448d096270464cc2617df84ac7
Richard Kanai(李凯男) <richardkanainjeri@gmail.com>
2023-10-28 15:30:16 +0300

    Fix update 09

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
         <div className='w-full py-4 px-10 flex justify-between align-middle text-sky-100 bg-sky-600 rounded-b '>
             <Link className='font-bold text-xl hover:text-sky-300' href='/' >MongoTopics</Link>
             <div className="flex align-middle gap-4">
-                <Link href={`${process.env.BASEURL}/Add`} className='text-lg hover:text-sky-300'>Add</Link>
+                <Link href={`/Add`} className='text-lg hover:text-sky-300'>Add</Link>
                 <button className='p-1 rounded-lg bg-sky-800 hover:bg-sky-900'>Profile</button>
             </div>
         </div>
